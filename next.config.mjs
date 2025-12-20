99d716b49edddab1700e17a50c1667867a9162cd
Niko Yu <137782638+thesleepyniko@users.noreply.github.com>
2025-12-20 18:51:59 -0500

    Fix for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -11,15 +11,10 @@ const nextConfig = {
     minimumCacheTTL: 60 * 60 * 24 * 365, // 1 year
     deviceSizes: [640, 750, 828, 1080, 1200],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    nextImageExportOptimizer: {
-      enabled: true,
-      cacheFolderRelativeToPublicFolder: 'cache',
-    },
   },
   compress: true,
   poweredByHeader: false,
   productionBrowserSourceMaps: false,
-  swcMinify: true,
   experimental: {
     optimizePackageImports: ['lucide-react'],
   },
@@ -31,10 +26,6 @@ const nextConfig = {
           {
             key: 'Cache-Control',
             value: 'public, max-age=31536000, immutable',
-          },
-          {
-            key: 'Content-Type',
-            value: 'image/jpeg',
           },
         ],
       },
