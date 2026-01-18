9c38640c487bda639d7baa5b9f59666fcb8d89d0
Niko Yu <137782638+thesleepyniko@users.noreply.github.com>
2026-01-17 21:09:29 -0500

    Refactor code structure for improved readability and maintainability

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -30,7 +30,7 @@ export default function Component() {
             <h2 className="text-2xl font-bold">Meet the people who make it all possible:</h2>
         </div>
         <div className="flex flex-col items-center w-full space-y-6 px-4">
-            <h2 className="text-3xl font-bold mb-4">Coaches and Metors</h2>
+            <h2 className="text-3xl font-bold mb-4">Coaches and Mentors</h2>
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full max-w-5xl mt-6">
                 <Bio
@@ -47,14 +47,38 @@ export default function Component() {
                 <Bio
                     img="/images/coach.jpg"
                     name="Haitao Dai"
-                    teamRole="Coach and Team Lead"
+                    teamRole="Head Coach"
                     text="Hello! I’m Haitao Dai, and this is my third year coaching the Ingenuity FTC team. I earned my PhD in Electrical Engineering from Boston University and currently work as a Senior Staff Engineer at Northrop Grumman. I’m passionate about circuit design, control systems, and automation, and I enjoy sharing that enthusiasm with our students. My goal is to guide the team through the entire engineering process—from concept and design to testing and refinement—while ensuring they have the support and resources they need to learn, grow, and succeed."
                     pronouns="he/him"
                     href="#"
                     className="flex justify-center items-center"
                 />
+                <Bio
+                    img="/images/baoshe.jpg"
+                    name="Baoshe Zhang"
+                    teamRole="Software Mentor"
+                    text="Dr. Baoshe Zhang is a software mentor for Team Ingenuity. He holds a Ph.D. in Physics from the Hong Kong University of Science and Technology and is an Associate Professor at the University of Maryland School of Medicine. With a background in software engineering and expertise in programming and automation, he helps guide the team's software development."
+                    pronouns="he/him"
+                    href="#"
+                    className="col-span-full flex justify-center items-center"
+                />
             </div>
             {/* <nav className="p-4 backdrop-blur-md bg-black/30 fixed w-full z-50"> */}
+        </div>
+        <div className="flex flex-col items-center w-full space-y-6 px-4">
+            <h2 className="text-3xl font-bold mb-4">Team Leadership</h2>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full max-w-5xl mt-6">
+                <Bio
+                    img="/images/elaine.jpg"
+                    name="Elaine"
+                    teamRole="Team Captain"
+                    text="Elaine is a sophomore and third-year team member with seven years of experience in FIRST. With past experience in hardware, software, and outreach, she is eager to assist her team in any aspect. In her free time, you can find Elaine hiding in a corner or managing her minions."
+                    pronouns="she/her"
+                    href="#"
+                    className="col-span-full flex justify-center items-center"
+                />
+            </div>
         </div>
         <div className="flex flex-col items-center w-full space-y-6 px-4">
             <h2 className="text-3xl font-bold mb-4">Software Team</h2>
@@ -71,7 +95,7 @@ export default function Component() {
                 <Bio
                     img="/images/rainger.webp"
                     name="Rainger"
-                    teamRole="Software Team"
+                    teamRole="Software"
                     text="Rainger works on the software side of Team Ingenuity, having worked on the robot's vision system along with maintaing and building out the website as it stands today. In his free time, he likes to code projects, mostly in Python or Javascript, and work on running events with Hack Club."
                     pronouns="he/him"
                     href="#"
@@ -86,7 +110,7 @@ export default function Component() {
                     <Bio
                     img="/images/erik.jpg"
                     name="Erik"
-                    teamRole="Hardware Lead"
+                    teamRole="Mechanical Lead"
                     text="Erik is the Hardware Lead of Team Ingenuity, overseeing the mechanical design and construction of the robot. With a keen eye for detail and a passion for engineering, Erik ensures that the robot is built to perform at its best during competitions. Erik is currenlty a sophmore at Gilman and enjoys playing squash in his free time."
                     pronouns="he/him"
                     href="#"
@@ -95,7 +119,7 @@ export default function Component() {
                 <Bio
                     img="/images/nathan.jpeg"
                     name="Nathan"
-                    teamRole="Hardware Team"
+                    teamRole="Electrical Lead"
                     text="Nathan is a 3rd year FTC student on team Ingenuity 24220. He also participated in 4 years of FLL even going to nationals once. He enjoys playing soccer and spending times with his friends and family."
                     pronouns="he/him"
                     href="#"
@@ -104,11 +128,20 @@ export default function Component() {
                 <Bio
                     img="/images/sherry1.png"
                     name="Sherry"
-                    teamRole="Hardware Team"
+                    teamRole="Hardware"
                     text="Sherry works on the hardware and portfolio, where she leads design initiatives, active building, and portfolio development. In her free time, she enjoys reading, fencing, and exploring cool concepts."
                     pronouns="she/her"
                     href="#"
-                    className="col-span-full flex justify-center items-center"
+                    className="flex justify-center items-center"
+                />
+                <Bio
+                    img="/images/vincent.jpg"
+                    name="Vincent"
+                    teamRole="Hardware"
+                    text="Vincent is a dedicated member of Team Ingenuity, contributing his skills and passion to our projects. With a keen interest in robotics and engineering, he plays a vital role in our team's success."
+                    pronouns="he/him"
+                    href="#"
+                    className="flex justify-center items-center"
                 />
             </div>
             {/* <nav className="p-4 backdrop-blur-md bg-black/30 fixed w-full z-50"> */}
@@ -130,13 +163,13 @@ export default function Component() {
             {/* <nav className="p-4 backdrop-blur-md bg-black/30 fixed w-full z-50"> */}
         </div>
         <div className="flex flex-col items-center w-full space-y-6 px-4">
-            <h2 className="text-3xl font-bold mb-4">General Support Team</h2>
+            <h2 className="text-3xl font-bold mb-4">Outreach & Strategy</h2>
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full max-w-5xl mt-6">
                 <Bio
                     img="/images/max.jpg"
                     name="Max"
-                    teamRole="General Support Co-Lead"
+                    teamRole="Outreach"
                     text="Max is a dedicated member of Team Ingenuity, contributing his skills and passion to our projects. With a keen interest in robotics and engineering, he plays a vital role in our team's success."
                     pronouns="he/him"
                     href="#"
@@ -145,20 +178,11 @@ export default function Component() {
                 <Bio
                     img="/images/jonathan.jpeg"
                     name="Jonathan"
-                    teamRole="General Support Co-Lead"
+                    teamRole="Strategy"
                     text="Jonathan, a new addition to Team Ingenuity, focuses on the completion of various tasks necessary for the team, ranging from hardware roles to mission strategy. In his downtime, he enjoys associating with friends and family."
                     pronouns="he/him"
                     href="#"
                     className="flex justify-center items-center"
-                />
-                <Bio
-                    img="/images/vincent.jpg"
-                    name="Vincent"
-                    teamRole="General Support Member"
-                    text="Vincent is a dedicated member of Team Ingenuity, contributing his skills and passion to our projects. With a keen interest in robotics and engineering, he plays a vital role in our team's success."
-                    pronouns="he/him"
-                    href="#"
-                    className="col-span-full flex justify-center items-center"
                 />
             </div>
         </div>
